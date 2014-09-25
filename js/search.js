e47f41cccd079c99dd8356e7c830c201775eef1c
mohit <mohit.11.arora@gmail.com>
2014-09-25 22:09:42 +0530

    more changes

--- js/search.js
+++ js/search.js
@@ -1,4 +1,14 @@
 $(function(){
 	$('body').search();
 	
+	function changebanner(){
+		
+		if($('.basetop').hasClass('basetop1')){		
+			$('.basetop').removeClass('basetop1').find('ul').show();
+		}else{
+			$('.basetop').addClass('basetop1').find('ul').hide();
+		}
+		setTimeout(changebanner,6000)
+	}
+	setTimeout(changebanner,6000)
 });
